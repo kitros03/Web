63c7501f0d6123293df629ab58026086734a3eaa
Rafas17-tp <raphael17.2003cy@gmail.com>
2025-08-19 18:16:03 +0300

    Create secretarydashboard.js

---
+++ secretarydashboard.js
@@ -0,0 +1,15 @@
+const container = document.getElementById('dashboard-container');
+
+document.getElementById('viewstatsBtn').onclick = () => {
+  window.location.href = 'viewstatsBtn.php';
+};
+document.getElementById('studentassignmentBtn').onclick = () => {
+  window.location.href = 'studentassignmentBtn.php';
+};
+document.getElementById('managethesesBtn').onclick = () => {
+  window.location.href = 'managethesesBtn.php';
+};
+document.getElementById('logoutBtn').onclick = () => {
+  window.location.href = 'logout.php';
+};
+
